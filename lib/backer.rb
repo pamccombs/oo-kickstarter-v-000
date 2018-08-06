813049ba7c7833102db4c3c1f955ab2f2b6e86b1
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-06 21:00:12 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  attr_accessor :name
+  
+  
+  
+  def initialize(name)
+  @name= name
+  @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    
+  end
+  
+end
